bc1424c0b3aca931c4b336c082178171fed3bde3
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-28 14:54:55 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,8 +8,7 @@ def key_for_min_value(name_hash)
     if lowest_value = nil || lowest_value > value
       lowest_value = value
       lowest_key = key
+    end
   end
-
-  key_list[0]
-
+  lowest_key
 end
