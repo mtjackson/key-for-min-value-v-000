77455eae1eb9b39a36d70e2a275a6bd784575237
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-28 14:49:14 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,19 +5,6 @@ def key_for_min_value(name_hash)
   key_list = []
   value_list = []
   name_hash.collect do |key, value|
-    value_list << value
-    key_list << key
-  end
-
-  i = 0
-  k = 0
-  smallest = value_list[0]
-  value_list.each do |value|
-    if value == smallest
-      i +=1
-    elsif value < smallest
-      smallest = value
-      i +=1
   end
 
   key_list[0]
